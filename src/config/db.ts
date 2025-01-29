a4a8b254787c68a46c918c8085f210c812feeba9
yoavhizkyia <yoavhizkyia@gmail.com>
2025-01-29 16:07:08 +0200

    sing up and login logic and test

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,10 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 
-export const client = new Client({
+export const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   password: '1234',
   port: 5433,
 });
+
+export default pool;
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,13 @@
-import express, { Router, json } from 'express';
-import { client } from './config/db';
+import express, { json } from 'express';
+import v1Router from './routers/v1';
 
 const app = express();
 app.use(json());
 
-const userRouter = Router();
-
-const createUser = 'INSERT INTO users (user_name, email, password) VALUES ($1, $2, $3)';
-
-userRouter.get('/', async (req, res) => {
-  try{
-    await client.connect();
-    const res1 = await client.query('SELECT * FROM users')
-    console.log(res1.rows);
-    res.sendStatus(200).json(res1.rows);
-  }
-  catch(err){
-    console.error(err);
-  }
-  finally{
-    client.end();
-    res.sendStatus(500)
-  }
-});
-
-userRouter.post('/signIn', async (req, res) => {
-  const { userName, email, password } = req.body;
-  
-  try{
-    await client.connect();
-    // bycrypt password
-    // login
-  }
-  catch(err){
-    console.error(err);
-  }
-})
-
-app.use('/users', userRouter);
+app.use('/v1', v1Router);
 
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
+
+export default app;
